1d58a5d1cc0ca9260c5759a2f2969147e958ec14
Parsons <Robert.Parsons@mines.sdsmt.edu>
2017-10-06 14:42:44 -0600

    RobotMap already exists and is the standard

--- src/CommandBase.cpp
+++ src/CommandBase.cpp
@@ -3,7 +3,7 @@
 #include <Commands/Scheduler.h>
 
 #include "Subsystems/MecanumDriveTrain.h"
-#include "HardwareConfiguration.h"
+#include "RobotMap.h"
 
 // Initialize a single static instance of all of your subsystems. The following
 // line should be repeated for each subsystem in the project.
